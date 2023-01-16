6c4608c6330edd579fd8941298b87eba4b6c0a1a
David Rempel <ddrempti@gmail.com>
2023-01-16 17:33:44 -0600

    Started branch

---
+++ launt-siekja/context.py
@@ -0,0 +1,8 @@
+OUTPUT_DIR = "~/Documents/launt-siekja"
+
+class Environment:
+    website_config = None
+    output_directory = OUTPUT_DIR
+    google_sheets_config = None
+    cache_directory = None
+    last_update = None
